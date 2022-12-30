9da8f71ad8df555242e7f8ee06b4827367c2a0e8
asus <asus@DESKTOP-J4576LJ>
2022-12-30 14:37:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            int f = 0;
+            Console.WriteLine("Введите строку символов");
+            str = Console.ReadLine(); //Переводим строчку в массив Char
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == str[i + 1])
+                    f = 1;
+            }
+
+            if (f == 1)
+                Console.WriteLine("Есть одинаковые символы 44");
+            else
+                Console.WriteLine("Нет одинаковых символов или не 44");
+
+            Console.ReadLine();
+        }
+    }
+}
